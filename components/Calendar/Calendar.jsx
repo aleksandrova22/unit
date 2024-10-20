e85bad4dd360fc9c6d11912e1493670040a7c9c8
SergMt <66214453+Serg-Mt@users.noreply.github.com>
2024-10-20 13:53:30 +0400

    конец занятия 20.10

--- components/Calendar/Calendar.jsx
+++ components/Calendar/Calendar.jsx
@@ -1,44 +1,51 @@
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { LocaleContext } from './LocaleContext';
-import classes from './Calendar.module.css';
+import defaultclasses from './Calendar.module.css';
 
-export function Calendar({ date }) {
+export function Calendar({ date, classes = defaultclasses }) {
   const
     locale = useContext(LocaleContext),
+    dayNames = useMemo(() => Array.from({ length: 7 }, (_, index) => <td key={index}>{(new Date(2019, 0, index)).toLocaleDateString(locale, { weekday: 'short' })}</td>), [locale]),
     caption = date.toLocaleDateString(locale, { month: 'long', year: 'numeric' }),
     year = date.getFullYear(),
     month = date.getMonth(),
+    selected = date.getDate(),
     max = (new Date(year, month + 1, 0)).getDate(), // число предшествующее 1 числу (нулевое) следующего месяца и есть число дней в текущем
     firstDayOfWeek = (new Date(year, month, 1)).getDay(),   // ВС=0 ПН=1 ВТ=2 .. СБ=6 
     shift = (-1 + firstDayOfWeek + 7) % 7;                  //      ПН=0 ВТ=1 .. СБ=5 ВС=6
   return <>
     <table className={classes.calendar}>
       <caption>{caption}</caption>
+      <thead>
+        <tr>{dayNames}</tr>
+      </thead>
       <tbody>
-        <Month shift={shift} max={max} />
+        <Month shift={shift} max={max} selected={selected} />
       </tbody>
     </table>
   </>;
-}
-
-function Month({ shift, max }) {
-  const
-    result = [];
-  for (let start = 1 - shift; start <= +max; start += 7) {
-    result.push(<Week key={start} start={start} max={max} />)
-  }
-  return <>{result}</>;
-}
 
 
-function Week({ start, max }) {
-  return <tr>
-    {Array.from({ length: 7 }, (_, index,) => {
-      const day = start + index;
-      return <td key={index}>
-        {day >= 1 && day <= max && day}
-      </td>;
+  function Month({ shift, max }) {
+    const
+      result = [];
+    for (let start = 1 - shift; start <= +max; start += 7) {
+      result.push(<Week key={start} start={start} max={max} selected={selected} />)
     }
-    )}
-  </tr>
+    return <>{result}</>;
+  }
+
+
+  function Week({ start, max }) {
+    return <tr>
+      {Array.from({ length: 7 }, (_, index,) => {
+        const
+          day = start + index;
+        return <td key={index} data-day={day} className={day === selected ? classes.selected : ''}>
+          {day >= 1 && day <= max && day}
+        </td>;
+      }
+      )}
+    </tr>
+  }
 }
